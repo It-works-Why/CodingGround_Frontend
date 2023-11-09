b2406a8c1e331685a58b88ec7c7056cc2609cc50
Claire-Winz <airspirk@gmail.com>
2023-11-09 15:39:01 +0900

    [💄CSS]Logo.vue CSS 분리

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,20 +2,7 @@
   <p class="d-inline-block logo px-3"><img class="logo_img" src="../assets/img/Logo.png" @click="this.$router.push('/')"></p>
 </template>
 
-<style scoped>
-.logo_img {
-  object-fit: contain;
-  width: 288px;
-  height: 123px;
-  cursor: pointer;
-}
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-</style>
-
+<style src="../assets/css/components/Logo.css" scoped/>
 
 <script>
 export default{
