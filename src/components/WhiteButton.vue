09d985bb37ab3ce5ae82f269a09965dd0879f435
realomadia <hhun6840@naver.com>
2023-11-10 09:25:10 +0900

    [:sparkles: DEV] 회원가입 프론트 구현

---
+++ src/components/WhiteButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <input class="fw-bold px-3 py-2 border-0 rounded-4" type="button" :value="buttonValue">
+</template>
+
+<style scoped>
+
+
+</style>
+
+
+<script>
+export default{
+  name:"WhiteButton",
+  data(){
+    return{
+    }
+  },
+  methods: {
+
+  },
+  props: {
+    buttonValue: {
+      type: String,
+      default: '확인',
+    },
+  }
+}
+</script>
