54e45d1fc09c651c7470be30e1b07b624f9311f6
realomadia <hhun6840@naver.com>
2023-11-10 12:34:19 +0900

    [:sparkles: DEV] WarningAlert 추가 모든 alert 전역 메소드에 추가

--- src/assets/js/alert.js
+++ src/assets/js/alert.js
@@ -17,11 +17,30 @@ const methods = {
         successMessageElem.innerText = successMessage;
         const toast = new Toast(successMessageToast);
         toast.show();
-    }
+    },
+    errorMessageToast(errorMessage) {
+        const errorMessageToast = document.getElementById('errorMessageToast');
+        const errorMessageElem = document.getElementById('errorMessage');
+
+        errorMessageElem.innerText = errorMessage;
+        const toast = new Toast(errorMessageToast);
+        toast.show();
+    },
+    warningMessageToast(warningMessage) {
+        const warningMessageToast = document.getElementById('warningMessageToast');
+        const warningMessageElem = document.getElementById('warningMessage');
+
+        warningMessageElem.innerText = warningMessage;
+        const toast = new Toast(warningMessageToast);
+        toast.show();
+    },
 }
+
 
 export default {
     install(Vue) {
         Vue.config.globalProperties.$successAlert = methods.successMessageToast;
+        Vue.config.globalProperties.$errorAlert = methods.errorMessageToast;
+        Vue.config.globalProperties.$warningAlert = methods.warningMessageToast;
     },
 };
