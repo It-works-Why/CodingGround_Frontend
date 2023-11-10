28a71d9e29f5dd223664c64502f33616cf3ea1ae
shshe22 <shshe22@naver.com>
2023-11-10 14:35:32 +0900

    design : battleListView 수정 3

--- src/view/BattleListView.vue
+++ src/view/BattleListView.vue
@@ -12,7 +12,7 @@
               <div class="col-lg-3 col-md-6">
                 <div class="service-box red" @click="modalOpen('RANK GAME')">
                   <i class="bi bi-file icon">
-                    <img class="img" src="../assets/img/Battle1.png">
+                    <img class="img" src="../assets/img/Battle1.png" alt="RANK GAME IMG">
                   </i>
                   <h3>RANK GAME</h3>
                 </div>
@@ -20,7 +20,7 @@
               <div class="col-lg-3 col-md-6">
                 <div class="service-box red" @click="modalOpen('MATCHING')">
                   <i class="bi bi-pencil-fill icon">
-                    <img class="img" src="../assets/img/Battle2.png">
+                    <img class="img" src="../assets/img/Battle2.png" alt="MATCHING IMG">
                   </i>
                   <h3>MATCHING</h3>
                 </div>
@@ -47,8 +47,8 @@
         <div>
           <p class="modal-title">대결 언어</p>
           <div class="modal-box">
-            <img class="langImg" v-if="langImg === 'java'" src="@/assets/img/Java.png">
-            <img class="langImg" v-else-if="langImg === 'c'" src="@/assets/img/Camera.png">
+            <img class="langImg" v-if="langImg === 'java'" src="@/assets/img/Java.png" alt="JAVA IMG">
+            <img class="langImg" v-else-if="langImg === 'c'" src="@/assets/img/Camera.png" alt="C IMG">
             <select v-model="langImg" class="modal-select-box">
               <option value="" disabled selected hidden>언어를 선택해주세요.</option>
               <option value="java">java</option>
