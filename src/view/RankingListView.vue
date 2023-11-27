d89948d3af7531dac8798ac11615b36173a98177
Kyoungwookkim <uk7764@naver.com>
2023-11-27 19:00:17 +0900

    ✨: RankingList 출력 기능 구현

--- src/view/RankingListView.vue
+++ src/view/RankingListView.vue
@@ -19,12 +19,12 @@
     </table>
     <table :key="i" :value="userRanking" v-for="(userRanking, i) in userRankingList" class="mt-1 text-white list_box m-auto">
       <tr>
-        <td class="ranking">{{userRanking.ranking}}</td>
+        <td class="ranking">{{userRanking.rankOrder}}</td>
         <td class="profileimg px-2"><img :src="require(`@/assets/img/DefaultProfile.png`)" width="35"></td>
-        <td class="nickname">{{userRanking.nickname}}</td>
-        <td class="tier"><img :src="require(`@/assets/img/tier/${userRanking.tier}.png`)" width="35"></td>
-        <td class="rankpoint">{{userRanking.rankpoint}} RP</td>
-        <td class="winrate">{{userRanking.winrate}}%</td>
+        <td class="nickname">{{userRanking.userNickname}}</td>
+        <td class="tier"><img :src="require(`@/assets/img/tier/${userRanking.rankNum}.png`)" width="35"></td>
+        <td class="rankpoint">{{userRanking.rankScore}} RP</td>
+        <td class="winrate">{{userRanking.recordPercentage}}%</td>
       </tr>
     </table>
   </div>
@@ -46,52 +46,21 @@ export default {
           "2023-8",
       ],
       searchInput: "",
-      userRankingList: [
-        {
-          ranking: '1',
-          profileImg: '',
-          nickname: '치치는치치야',
-          tier: '1',
-          rankpoint: '2000',
-          winrate: '99.9'
-        },
-        {
-          ranking: '2',
-          profileImg: '',
-          nickname: '에이스못먹는치치',
-          tier: '2',
-          rankpoint: '1500',
-          winrate: '88.8'
-        },
-        {
-          ranking: '3',
-          profileImg: '',
-          nickname: '이게_왜_되지',
-          tier: '3',
-          rankpoint: '1000',
-          winrate: '77.7'
-        },
-        {
-          ranking: '4',
-          profileImg: '',
-          nickname: '에이스안먹는치치',
-          tier: '4',
-          rankpoint: '500',
-          winrate: '66.6'
-        },
-        {
-          ranking: '5',
-          profileImg: '',
-          nickname: 'lomadia',
-          tier: '5',
-          rankpoint: '100',
-          winrate: '0.1'
-        }
-
-      ],
-
+      userRankingList: [],
     }
-  }
+  },
+
+  methods: {
+    load() {
+      this.$httpUtil('/user/ranking/list', 'GET', null, (data) => {
+        console.log(data);
+        this.userRankingList = data;
+      })
+    }
+  },
+  mounted() {
+    this.load();
+  },
 
 }
 </script>
