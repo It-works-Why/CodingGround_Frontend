185783bea414f99e8c692a7fec42b7172aaef757
Prkhgwan <crer03@gmail.com>
2023-12-06 09:50:34 +0900

    :green_heart: 파이프라인 수정 및 백엔드 주소 변경

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8090/',
-                // target: 'http://ace3c9fc9de9248f9adafd7fbf98953f-369489494.ca-central-1.elb.amazonaws.com:8090/',
+                // target: 'http://127.0.0.1:8090/',
+                target: 'http://ace3c9fc9de9248f9adafd7fbf98953f-369489494.ca-central-1.elb.amazonaws.com:8090/',
                 ws: true,
                 changeOrigin: true
             }
