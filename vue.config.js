e7f7c7f4e5049b32a59f20ddb2c81404aae1d74c
Prkhgwan <crer03@gmail.com>
2023-12-06 11:25:52 +0900

    :bug: api 오류 픽스 테스트

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         proxy: {
             '/api': {
                 // target: 'http://127.0.0.1:8090/',
-                target: 'http://ace3c9fc9de9248f9adafd7fbf98953f-369489494.ca-central-1.elb.amazonaws.com:8090',
+                target: 'http://ace3c9fc9de9248f9adafd7fbf98953f-369489494.ca-central-1.elb.amazonaws.com:8090/api/',
                 ws: true,
                 changeOrigin: true
             }
