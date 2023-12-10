7a208dce2ba7a91aa245a9a4082652b3b12d986e
Winz <airspirk@gmail.com>
2023-12-10 21:08:00 +0900

    httpss

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -7,7 +7,8 @@ import { errorMessageToast } from "@/assets/js/alert";
 // 기존에 설정한 axiosInstance에 토큰 추가하기
 const axiosInstance = axios.create({
     timeout: 10000, // 타임아웃 설정 (10초)
-    baseURL: "http://k8s-backendgroup-faceca018f-1950602437.ca-central-1.elb.amazonaws.com", //"https://api.mzc-codingground.click/api",
+    // baseURL: "http://k8s-backendgroup-faceca018f-1950602437.ca-central-1.elb.amazonaws.com",
+    baseURL: "https://api.mzc-codingground.click/api",
     headers: {
         "Content-Type": "application/json",
 /*        'X-RapidAPI-Key': '92645c8b10msh7f8b72338057770p1deaecjsn641c7097464c',
