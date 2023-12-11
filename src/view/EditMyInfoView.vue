3168a2fde4a5835b9d33b0a3dc3d2a87df3c14bb
shshe22 <shshe22@naver.com>
2023-12-11 11:18:17 +0900

    :rotating_light:  fix : 내 정보 수정 프로필 업로드 실패 예외 처리

--- src/view/EditMyInfoView.vue
+++ src/view/EditMyInfoView.vue
@@ -195,7 +195,9 @@ export default {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
-      }).then(() => {}).catch(() => {})
+      }).then(() => {}).catch(() => {
+        this.$errorAlert("프로필 업로드에 실패했습니다.");
+      })
 
     },
     changeImg(event) {
